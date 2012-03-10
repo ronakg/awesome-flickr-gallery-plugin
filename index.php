a7d43858611f547286f4cbc957b8ef047adf47de
Ronak Gandhi <ronak.gandhi@ronakg.com>
2012-03-11 00:30:40 +0530

    Add capability to update plugin from host other than WordPress.org.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
    Plugin Name: Awesome Flickr Gallery
    Plugin URI: http://www.ronakg.com/projects/awesome-flickr-gallery-wordpress-plugin/
    Description: Awesome Flickr Gallery is a simple, fast and light plugin to create a gallery of your Flickr photos on your WordPress enabled website.  This plugin aims at providing a simple yet customizable way to create stunning Flickr gallery.
-   Version: 3.3.2
+   Version: 3.3.3
    Author: Ronak Gandhi
    Author URI: http://www.ronakg.com
    License: GPL2
@@ -23,6 +23,96 @@
    along with this program; if not, write to the Free Software
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
+// TEMP: Enable update check on every request. Normally you don't need this! This is for testing only!                                                                    
+//set_site_transient('update_plugins', null);
+
+// TEMP: Show which variables are being requested when query plugin API
+ /*
+add_filter('plugins_api_result', 'afg_aaa_result', 10, 3);
+function afg_aaa_result($res, $action, $args) {
+    print_r($res);
+    return $res;
+}
+  */
+
+$api_url = 'http://www.ronakg.com/update_plugin/';
+$plugin_slug = basename(dirname(__FILE__));
+$plugin_slug_file = basename(__FILE__);
+
+// Take over the update check
+add_filter('pre_set_site_transient_update_plugins', 'afg_check_for_plugin_update');
+
+function afg_check_for_plugin_update($checked_data) {
+	global $api_url, $plugin_slug, $plugin_slug_file;
+	
+	if (empty($checked_data->checked))
+		return $checked_data;
+	
+	$request_args = array(
+        'slug' => $plugin_slug,
+        'version' => $checked_data->checked[$plugin_slug .'/'. $plugin_slug_file],
+        'package_type' => 'stable',
+	);
+
+	$request_string = afg_prepare_request('basic_check', $request_args);
+	
+	// Start checking for an update
+	$raw_response = wp_remote_post($api_url, $request_string);
+	
+	if (!is_wp_error($raw_response) && ($raw_response['response']['code'] == 200))
+		$response = unserialize($raw_response['body']);
+	
+	if (is_object($response) && !empty($response)) // Feed the update data into WP updater
+		$checked_data->response[$plugin_slug .'/'. $plugin_slug_file] = $response;
+	
+	return $checked_data;
+}
+
+// Take over the Plugin info screen
+add_filter('plugins_api', 'my_plugin_api_call', 10, 3);
+
+function my_plugin_api_call($def, $action, $args) {
+	global $plugin_slug, $api_url, $plugin_slug_file;
+	
+	if ($args->slug != $plugin_slug)
+		return false;
+	
+	// Get the current version
+	$plugin_info = get_site_transient('update_plugins');
+	$current_version = $plugin_info->checked[$plugin_slug .'/'. $plugin_slug_file];
+	$args->version = $current_version;
+	
+	$request_string = afg_prepare_request($action, $args);
+	
+	$request = wp_remote_post($api_url, $request_string);
+	
+	if (is_wp_error($request)) {
+		$res = new WP_Error('plugins_api_failed', __('An Unexpected HTTP Error occurred during the API request.</p> <p><a href="?" onclick="document.location.reload(); return false;">Try again</a>'), $request->get_error_message());
+	} else {
+		$res = unserialize($request['body']);
+		
+		if ($res === false)
+			$res = new WP_Error('plugins_api_failed', __('An unknown error occurred'), $request['body']);
+	}
+	
+	return $res;
+}
+
+
+function afg_prepare_request($action, $args) {
+	global $wp_version;
+	
+	return array(
+		'body' => array(
+			'action' => $action, 
+			'request' => serialize($args),
+			'api-key' => md5(get_bloginfo('url'))
+		),
+		'user-agent' => 'WordPress/' . $wp_version . '; ' . get_bloginfo('url')
+	);	
+}
+
 require_once('afgFlickr/afgFlickr.php');
 include_once('admin_settings.php');
 include_once('afg_libs.php');
