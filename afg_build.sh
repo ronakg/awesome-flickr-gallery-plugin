79b8173f043fb473e486da7cc08c9a005478a704
Ronak Gandhi <ronak.gandhi@ronakg.com>
2012-03-13 17:44:51 +0530

    Removed executable mode

--- afg_build.sh
+++ afg_build.sh
@@ -1,3 +1,4 @@
 # Build a zip file for current version of the plugin. Takes version number as argument for zipfile name.
-[ $# -eq 0 ] && { echo "Usage: $0 version_no"; exit 1; }
-zip -r awesome-flickr-gallery-plugin-$1.zip . -x \*.git\* -x \*timthumb.txt\*
+branch=`git branch | grep \*`
+branch=${branch:2}
+zip -r awesome-flickr-gallery-plugin-$branch.zip . -x \*.git\* -x \*timthumb.txt\*
